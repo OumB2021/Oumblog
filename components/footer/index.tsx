68e0065c511ea0a77d89ebd29a8c505b0359d690
Oumb2021 <baarryoumar@gmail.com>
2025-02-11 19:45:14 -0500

    profile picture

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,15 +1,26 @@
+import { auth } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 
-function Footer() {
+async function Footer() {
+  const session = await auth();
+  if (!session || !session.user) {
+    return null;
+  }
   return (
     <div className="mt-10 flex flex-col md:flex-row gap-10 md:gap-0 justify-between items-center p-10">
       <div className="flex-1 flex flex-col gap-3">
-        <div className="flex items-center gap-2">
-          <Image src="/logo.png" alt="oumb blog" width={40} height={40} />
+        <div className="flex items-center gap-2 rounded-full">
+          <Image
+            src={session?.user?.image || ""}
+            alt="oumb blog"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
           <Link
             href="/"
-            className="font-bold text-xl text-zinc-700 hover:text-black"
+            className="font-bold text-xl text-zinc-700 hover:text-black "
           >
             OumBlog
           </Link>
